82ec35aab9260fd9c4c02393d7d44c2f778f4347
jogoshugh <jgough@JGough.corp.versionone.net>
2012-12-19 00:05:06 -0500

    - Began genericization into a simple Asset Editor and Lister - Moved fields, selection, asset name data out to client html page - Renamed variables to be "Asset" not "Request"-specific

--- VersionOne.Requestor.NET/versionOne.requestor.js
+++ VersionOne.Requestor.NET/versionOne.requestor.js
@@ -1,16 +1,19 @@
-function VersionOneRequestor (useServiceGateway, host, service, serviceGateway, versionOneAuth) {
+function VersionOneAssetEditor (useServiceGateway, host, service, serviceGateway,
+    versionOneAuth, assetName, fields, selectFields) {
     this.useServiceGateway = useServiceGateway;
     this.host = host;
     this.service = service;
     this.versionOneAuth = versionOneAuth;
+    this.assetName = assetName;
+    this.selectFields = selectFields;
     this.serviceGateway = serviceGateway;
-    this.config();
+    this.config(fields);
     this.initializeThenSetup();
 }
 
-VersionOneRequestor.prototype.config = function () {
+VersionOneAssetEditor.prototype.config = function (fields) {
     var projectName = "'System (All Projects)'";
-    var contentType = "haljson";
+    var contentType = 'haljson';
         
     this.config = {
         projectName: projectName,
@@ -23,102 +26,81 @@ VersionOneRequestor.prototype.config = function () {
             acceptFormat: contentType
         },
         headers: {
-            Authorization: "Basic " + btoa(this.versionOneAuth) // TODO: clean this up
+            Authorization: 'Basic ' + btoa(this.versionOneAuth) // TODO: clean this up
         },
         whereCriteria: {
             Name: projectName
         },
         whereParams: {
             acceptFormat: contentType,
-            sel: ""
+            sel: ''
         },
-        fields:
-            [
-                {
-                    name: "RequestedBy",
-                    label: "Requested By",
-                    required: true,
-                    autofocus: "autofocus",
-                    placeholder: "Your name",
-                    def: ""
-                }, {
-                    name: "Name",
-                    required: true,
-                    placeholder: "Brief request title",
-                    label: "Title",
-                    autofocus: "",
-                    def: ""
-                }, {
-                    name: "Description",
-                    label: "Description",
-                    placeholder: "Give enough detail to aid follow-up conversations",
-                    required: false,
-                    autofocus: "",
-                    def: "",
-                    type: "textarea",
-                    height: 200
-                }
-            ]
+        fields: fields
     };
 };
 
-VersionOneRequestor.prototype.initializeThenSetup = function () {
+VersionOneAssetEditor.prototype.initializeThenSetup = function () {
     if (this.useServiceGateway) {
         this.setup();
         return;
     }
-    var url = this.config.service + "Scope" + "?where=" + $.param(this.config.whereCriteria)
-        + "&" + $.param(this.config.whereParams);
-    console.log("initializeThenSetup: " + url);
+    var url = this.config.service + 'Scope' + '?where=' + $.param(this.config.whereCriteria)
+        + '&' + $.param(this.config.whereParams);
+    console.log('initializeThenSetup: ' + url);
     var that = this;
     $.ajax({
         url: url,
         headers: this.config.headers,
-        type: "GET"
+        type: 'GET'
     }).done(function (data) {
         if (data.length > 0) {
             that.config.projectScopeId = data[0]._links.self.id;
             that.setup();
         } 
         else {
-            console.log("No results for query: " + url);
+            console.log('No results for query: ' + url);
         }
     }).fail(function (ex) { console.log(ex); });
 };
 
-VersionOneRequestor.prototype.setup = function () {
+VersionOneAssetEditor.prototype.setup = function () {
     if (this.useServiceGateway) {
         this.config.host = this.config.serviceGateway;
-        this.config.service = this.config.host + "/";
+        this.config.service = this.config.host + '/';
     }
 
-    $("#requestForm").html($("#fieldsTemplate").render({
+    $('#assetForm').html($('#fieldsTemplate').render({
         fields: this.config.fields
     }));
-    $("#requestForm").validVal();
+    $('#assetForm').validVal();
 
     var that = this;
-    $("#run").click(function () {
-        that.createStory();
+    $('#create').click(function () {
+        that.createAsset(assetName);
     });
-    $("#reset").click(function () {
+    $('#reset').click(function () {
         that.resetForm();
     });
 
-    // Populate the all requests list
-    this.loadAllRequests();    
+    $('.new').click(function() {
+        that.newAsset();
+    });
+
+    // Populate the assets list
+    this.loadAssets(this.assetName, this.selectFields);
 
     $(document).on('pagebeforechange', function(event, ui) {
-        console.log(ui.toPage["0"].id);
+        console.log("Id:");
+        console.log(ui);
     });
 };
 
-VersionOneRequestor.prototype.loadAllRequests = function () {
-    console.log("loadAllRequests");
-    var url = this.getRequestUrl() + "&" + $.param({ 
-        'sel': 'Name,RequestedBy'
+VersionOneAssetEditor.prototype.loadAssets = function (assetName, selectFields) {
+    console.log('loadAssets');
+    var url = this.getAssetUrl(assetName) + '&' + $.param({ 
+        'sel': selectFields
     });
-    console.log("loadAllRequests: " + url);
+    console.log('loadAssets: ' + url);
     var request = { url: url };
     if (!this.useServiceGateway) {
         request.headers = this.config.headers;
@@ -128,48 +110,83 @@ VersionOneRequestor.prototype.loadAllRequests = function () {
         console.log(data);
         for(var i = 0; i < data.length; i++) {
             var item = data[i];
-            var templ = $("<li></li>");
-            templ.html($("#requestItemTemplate").render(item));
-            var link = templ.children(".requestItem").bind("click", function() {
-                that.editRequest($(this).attr("data-href"));
+            var templ = $('<li></li>');
+            templ.html($('#assetItemTemplate').render(item));
+            var link = templ.children('.assetItem').bind('click', function() {
+                that.editAsset($(this).attr('data-href'));
             });
-            $("#requests").append(templ);
+            $('#assets').append(templ);
         }        
-        $("#requests").listview("refresh");
+        $('#assets').listview('refresh');
     }).fail(function(ex) { console.log(ex); });    
 };
 
-VersionOneRequestor.prototype.getRequestUrl = function() {
-    var url = this.config.service + "Request" + "?" + $.param(this.config.queryOpts);
+VersionOneAssetEditor.prototype.newAsset = function() {    
+    $.mobile.changePage("#new");
+    this.resetForm();
+};
+
+VersionOneAssetEditor.prototype.getAssetUrl = function(assetName) {
+    var url = this.config.service + assetName + '?' + $.param(this.config.queryOpts);
     return url;
 };
 
-VersionOneRequestor.prototype.editRequest = function(href) {
-    var url = this.config.host + href + "?" + $.param(this.config.queryOpts);
+VersionOneAssetEditor.prototype.editAsset = function(href) {
+    var url = this.config.host + href + '?' + $.param(this.config.queryOpts);
     var request = { url: url };
+    if (!this.useServiceGateway) {
+        request.headers = this.config.headers;
+    }
+    var that = this;
+    $.ajax(request).done(function(data) {
+        that.enumFields(function(key, field) {
+            var val = data[key];
+            if (val != null && val != 'undefined') {
+                var els = $('#' + key);
+                console.log(els);
+                if (els.length > 0) {
+                    var el = $(els[0]);
+                    el.val(data[key]);
+                }             
+            }
+        });       
+        $.mobile.changePage('#new');
+    }).fail(function(ex) { console.log(ex); });
+};
+
+VersionOneAssetEditor.prototype.enumFields = function(callback) {
+    for(var i in this.config.fields) {
+        var field = this.config.fields[i];
+        var key = field.name;
+        callback(key, field);        
+    }
+};
+
+VersionOneAssetEditor.prototype.updateAsset = function(href) {
+    var url = this.config.host + href + '?' + $.param(this.config.queryOpts);
+    var request = { 
+        url: url,
+        type: 'POST'
+    };
     if (!this.useServiceGateway) {
         request.headers = this.config.headers;
     }
     $.ajax(request).done(function(data) {
         console.log(data);
-        $.mobile.changePage("#new");
-        $("#Name").val(data.Name);
-        $("#RequestedBy").val(data.RequestedBy);
-        $("#Description").val(data.Description);
     }).fail(function(ex) { console.log(ex); });
 };
 
-VersionOneRequestor.prototype.createStory = function () {
+VersionOneAssetEditor.prototype.createAsset = function (assetName) {
     var dtoResult = this.createDto();
     if (dtoResult[0] == true) {
         return;
     }
     this.clearErrors();
     var dto = dtoResult[1];
-    var url = this.getRequestUrl();
+    var url = this.getAssetUrl(assetName);
     var request = {
         url: url,
-        type: "POST",
+        type: 'POST',
         data: JSON.stringify(dto),
         contentType: this.config.contentType
     };
@@ -179,31 +196,32 @@ VersionOneRequestor.prototype.createStory = function () {
     return $.ajax(request).done(function(data) {
         console.log(data);
         var item;
-        item = $("<div></div>");
-        item.html($("#requestItemTemplate").render(data));
-        return $("#output").prepend(item);
+        item = $('<div></div>');
+        item.html($('#assetItemTemplate').render(data));
+        return $('#output').prepend(item);
     }).fail(function(ex) { console.log(ex); });
 };
 
-VersionOneRequestor.prototype.createDto = function () {
-    var data = $("#requestForm").trigger("submitForm");
+VersionOneAssetEditor.prototype.createDto = function (addProjectIdRef) {
+    if (addProjectIdRef !== false) {
+        addProjectIdRef = true;
+    }
+    var data = $('#assetForm').trigger('submitForm');
     if (!data) {
         return [true, null];
     }
-    var attributes;
-    attributes = {};
+    var attributes = {};
     var hasError = false;
-    $("#requestForm .inputField").each(function () {
-        var el;
-        el = $(this);
-        var id = el.attr("id");
+    $('#assetForm .inputField').each(function () {
+        var el = $(this);
+        var id = el.attr('id');
         var val = el.val();
-        var required = el.attr("data-required");
-        if (required == "true" && (val == null || val == "undefined" || val == "")) {
+        var required = el.attr('data-required');
+        if (required == 'true' && (val == null || val == 'undefined' || val == '')) {
             hasError = true;
-            var error = $("#err" + id);
-            var label = error.attr("data-label");
-            error.text(label + " is required");
+            var error = $('#err' + id);
+            var label = error.attr('data-label');
+            error.text(label + ' is required');
         }
         return attributes[id] = el.val();
     });
@@ -216,10 +234,11 @@ VersionOneRequestor.prototype.createDto = function () {
     return [hasError, attributes];
 };
     
-VersionOneRequestor.prototype.clearErrors = function() {
-    $(".error").text("");
+VersionOneAssetEditor.prototype.clearErrors = function() {
+    $('.error').text('');
 };
 
-VersionOneRequestor.prototype.resetForm = function() {
-    $("#requestForm")[0].reset();
+VersionOneAssetEditor.prototype.resetForm = function() {
+    console.log('resetForm');
+    $('#assetForm')[0].reset();
 };
