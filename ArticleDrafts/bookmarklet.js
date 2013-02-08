4a4398556298fcae413f242e5979d55f9623650a
Josh Gough <jsgough@gmail.com>
2013-02-08 15:21:34 -0800

    Create bookmarklet.js

---
+++ ArticleDrafts/bookmarklet.js
@@ -0,0 +1,4 @@
+(function() {
+var container = document.getElementById("container");
+container.innerHTML = "<h1>Hello Mark</h1><p>I was injected by a script. This is only the beginning!</p>";
+})();
