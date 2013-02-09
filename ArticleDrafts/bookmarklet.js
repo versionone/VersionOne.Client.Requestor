e1838f20a4671286ac1eac69594c6156f810c67b
Josh Gough <jsgough@gmail.com>
2013-02-08 19:16:32 -0500

    Update ArticleDrafts/bookmarklet.js

--- ArticleDrafts/bookmarklet.js
+++ ArticleDrafts/bookmarklet.js
@@ -1,3 +1,3 @@
 (function() {
-document.body.innerHTML = "<h1>Hello Mark</h1><p>I was injected by a script. This is only the beginning!</p>";
+document.write("<h1>Hello Mark</h1><p>I was injected by a script. This is only the beginning!</p>");
 })();
