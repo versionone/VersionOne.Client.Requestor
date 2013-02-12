b8d3ba0ed7c7eed9edcc6f6873c9d04e3836d715
jogoshugh <jgough@JGough.corp.versionone.net>
2013-02-12 12:14:22 -0500

    Embeddable boot script

---
+++ VersionOne.FeatureRequestor/dynamic/Scripts/mainEmbed.js
@@ -0,0 +1,45 @@
+require.config({
+	baseUrl: 'https://raw.github.com/versionone/VersionOne.Requestor.NET/master/VersionOne.FeatureRequestor/dynamic/Scripts'
+});
+
+requirejs.config({
+	// The shim allows these non-AMD scripts to participate
+	// in the AMDified loading for other modules
+    shim: {
+    	'underscore': {
+    		exports: '_'
+    	}
+        ,'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        }
+        ,'jsrender' : {
+        	deps: ['jquery']
+        }
+    }
+});
+
+require([       
+        'config',
+        'jquery',
+	    'require',
+        'backbone',
+        'backbone-forms',
+        'editors/list',
+        'templates/bootstrap', 
+    	'toastr',
+        'jsrender'
+    ],
+    function(
+        v1config,
+        v1assetEditor,
+        $)
+    {
+        $(document).ready(function () {
+            window.v1AssetEditor = new v1assetEditor(v1config);
+            window.v1AssetEditor.on("assetFormCreated", function(assetForm) {
+                window.vRequestForm = assetForm;
+            });
+        });
+	}
+);
