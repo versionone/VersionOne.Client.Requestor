d99095e6c5d5cc49cf1863ab8e77fa57a880ff17
jogoshugh <jgough@JGough.corp.versionone.net>
2013-02-12 02:26:08 -0500

    Trying DYNAMIC load

---
+++ VersionOne.FeatureRequestor/dynamic/Scripts/main.js
@@ -0,0 +1,45 @@
+require.config({
+	baseUrl: 'https://raw.github.com/versionone/VersionOne.Requestor.NET/master/VersionOne.FeatureRequestor/dynamic'
+});
+
+requirejs.config({
+	// The shim allows these non-AMD scripts to participate
+	// in the AMDified loading for other modules
+    shim: {
+    	'underscore': {
+    		exports: '_'
+    	}
+        ,'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        }
+        ,'jsrender' : {
+        	deps: ['jquery']
+        }
+    }
+});
+
+require([
+        'config',
+        'v1assetEditor',
+        'jquery',
+        'backbone',
+        'backbone-forms',
+        'editors/list',
+        'templates/bootstrap', 
+    	'toastr',
+        'jsrender'
+    ],
+    function(
+        v1config,
+        v1assetEditor,
+        $)
+    {
+    	$(document).ready(function () {
+    	    window.v1AssetEditor = new v1assetEditor(v1config);
+            window.v1AssetEditor.on("assetFormCreated", function(assetForm) {
+                window.v1RequestForm = assetForm;
+            });
+    	});
+    }
+);
